30fee758040074c87b053189f425f1df96015c78
Rodrigo Delgadillo <red_g4898@proton.me>
2022-10-27 00:46:17 +0000

    Extended colors from tailwind to add green tone of fas.

--- src/components/content/Navbar.jsx
+++ src/components/content/Navbar.jsx
@@ -4,7 +4,7 @@ import NavbarElement from "./NavbarElement";
 const Navbar = () => {
 	return (
 		<nav className="col-span-8 col-start-3 grid w-full grid-cols-1 grid-rows-2 gap-3 bg-zinc-200">
-			<h1 className="row-span-1 row-start-1 text-center font-neue text-2xl font-black text-emerald-800">
+			<h1 className="row-span-1 row-start-1 text-center font-neue text-2xl font-black text-fasgreen">
 				Tienda Ángeles de la Salud
 			</h1>
 			<section className="row-span-1 row-start-2 flex justify-between">
